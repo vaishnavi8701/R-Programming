c442ad2f0548fc34f235d98cba042ab0a433cd35
Vaishnavi Palanisamy <51691751+vaishnavi8701@users.noreply.github.com>
2020-12-06 20:03:09 +0530

    Create Simple Regression.R

---
+++ Simple Regression.R
@@ -0,0 +1,14 @@
+#Simple Linear Regression
+
+data <- read.csv( "https://raw.githubusercontent.com/AdiPersonalWorks/Random/master/student_scores%20-%20student_scores.csv", header=TRUE)
+data
+X<-data$Hours
+Y<-data$Scores
+relation<-lm(Y~X)
+relation
+summary(relation)
+plot(Y,X,col = "blue",main = "Hours & Scores Regression",
+     abline(lm(X~Y)),cex = 1.3,pch = 16,xlab = "Scores of student",ylab = "Hours spend")
+p<-data.frame(X=9.25)
+predict(relation,p)
+
